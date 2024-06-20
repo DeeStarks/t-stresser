beee67f73bf77cb63176182a628f9e48170468e3
Daniel Onoja <danielonoja246@gmail.com>
2024-06-20 22:30:46 +0100

    feat: upgrade to version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tstresser',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'matplotlib==3.9.0',
@@ -16,9 +16,9 @@ setup(
             'tstresser=tstresser.cli:main'
         ]
     },
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A CLI-based stress tester for APIs',
+    author='Daniel Onoja',
+    author_email='danielonoja246@gmail.com',
+    description='A CLI-based stress tester for web applications and APIs',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/deestarks/t-stresser',
